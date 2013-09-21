ee629c4aa8bb2bf4d5b413140cef562187674503
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-21 12:47:03 -0400

    create tag feature, bug: new tag not generating properly

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 class Article < ActiveRecord::Base
 	has_many :comments
-  attr_accessible :title, :body
+	has_many :taggings
+	has_many :tags, through: :taggings
+  attr_accessible :title, :body, :tag_list
+
+  def tag_list
+  	self.tags.collect do |tag|
+  	tag.name
+  	end.join(", ")  
+  end
+
+  def tag_list=(tags_string)
+  	tag_names = tags_string.split(",").collect {|s| s.strip.downcase }.uniq
+  	new_or_found_tags = tag_names.collect {|name| Tag.first_or_create(name: name) }
+  	self.tags =new_or_found_tags
+	end
 end
