173849981b297f73e527ad478098a44023588d32
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-21 11:24:19 -0400

    create article forms, partial not working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,37 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(params[:article])
+		@article.save
+
+		flash.notice = "Article #{@article.title} was created!"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article #{@article.title} was unpublished!"
+		redirect_to articles_path 
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update_attributes(params[:article])
+
+		flash.notice = "Article #{@article.title} was updated!"
+
+		redirect_to article_path(@article)
+	end
 end
